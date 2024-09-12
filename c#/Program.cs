46a86d1e5dc3312776e87629bcab06756bc3e845
Pedro Ivo <pi3virgula14@outlook.com>
2024-09-12 12:00:44 -0300

    demo shop

--- c#/Program.cs
+++ c#/Program.cs
@@ -13,12 +13,12 @@ namespace suri_demo
             
             // Creating a new session
             // Get your assistant_id in admin 
-            var response = generative.CreateSession(9999); // 
+            var response = generative.CreateSession(7);
             string sessionId = response.Result.Id;
 
             // Sending a new message
             // You can implements file send inside this method
-            var messageResponse = generative.SendMessage(sessionId, "Quero falar com uma atendente");
+            var messageResponse = generative.SendMessage(sessionId, "Quero blusas e canecas");
 
             foreach (var message in messageResponse.Result.Response)
             {
@@ -28,12 +28,37 @@ namespace suri_demo
                         Console.WriteLine("trigger:");
                         var dataJson = JsonConvert.SerializeObject(message.Data);
                         var sessionResponse = JsonConvert.DeserializeObject<TriggerResponse>(dataJson);
-                        if (sessionResponse.FunctionName == "direcionar_atendente")
+                        if (sessionResponse.FunctionName == "consulta_produto")
                         {
-                            // Triggered 
-                            // Redirects to human service or other
+                            // Write your query to get product and send to user
+                            Console.WriteLine("Consulta produto:");
+                            if (sessionResponse.Args.ContainsKey("produto"))
+                            {
+                                Console.WriteLine(sessionResponse.Args["produto"]);
+                            }
+
+                            continue;
                         }
                         
+                        if (sessionResponse.FunctionName == "adicionar_carrinho")
+                        {
+                            // Write your add product here
+                            Console.WriteLine("Adicione ao carrinho:");
+                            if (sessionResponse.Args.ContainsKey("produto"))
+                            {
+                                Console.WriteLine(sessionResponse.Args["produto"]);
+                            }
+
+                            continue;
+                        }
+                        
+                        if (sessionResponse.FunctionName == "finalizar_compra")
+                        {
+                            // Write your finalize here
+                            Console.WriteLine("Compra finalizada!");
+                            continue;
+                        }
+
                         Console.WriteLine(dataJson);
                         break;
                     case "text":
